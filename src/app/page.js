d4eaa5f5fefd777bd9015dbccd550205e830d575
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-05-11 18:25:13 -0400

    create toggle with useState

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+"use client"
 
 import { BiUserPlus } from 'react-icons/bi'
 import Table from '../../components/table';
 import Form from '../../components/form'
+import { useState } from 'react/cjs/react.development';
 
 export default function Home() {
+  const [visible, setVisible] = useState(false)
   return (
     <div>
       <main>
@@ -12,14 +15,18 @@ export default function Home() {
         </h1>
         <div className="container mx-auto flex justify-between py-4 border-b">
           <div className="left flex gap-3">
-            <button className="flex items-center bg-orange-500 py-2 px-4 text-white text-sm hover:bg-orange-400 hover:border-orange-600">
+            <button 
+              className="flex items-center bg-orange-500 py-2 px-4 text-white text-sm hover:bg-orange-400 hover:border-orange-600"
+              onClick={() => setVisible(!visible)}  
+            >
               Agregar empleado  <BiUserPlus  className='text-md ml-1'/>
             </button>
           </div>
         </div>
+        
         <div className='container mx-auto'>
           <div className='px-2 py-8'>
-            <Form />
+            {visible && <Form />}
           </div>
           <Table />
         </div>
