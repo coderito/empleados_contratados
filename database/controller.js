1b3e9cf1aaa27729492b08106867ccad48985274
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-05-13 20:29:23 -0400

    add controller put

--- database/controller.js
+++ database/controller.js
@@ -19,11 +19,26 @@ export async function postUser(req, res){
     try {
         const formData = req.body;
         if(!formData) return res.status(404).json( { error: "Form Data Not Provided...!"});
-        
+
         const newUser = await Users.create( formData)
         return res.status(200).json(newUser)
         
     } catch (error) {
         return res.status(404).json({ error })
+    }
+}
+
+// put : http://localhost:3000/api/users
+export async function putUser(req, res){
+    try {
+        const {userId} = req.query
+        const formData = req.body
+
+        if(userId && formData){
+            const user = await Users.findByIdAndUpdate(userId, formData)
+            res.status(200).json(user)
+        }
+    } catch (error) {
+        res.status(404).json({error: "Error while updating the data"})
     }
 }
