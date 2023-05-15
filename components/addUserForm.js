494cf4a53e085a0e6db032c2d159121b23b8356a
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-05-15 19:27:19 -0400

    changing class for className

--- components/addUserForm.js
+++ components/addUserForm.js
@@ -78,8 +78,8 @@ export default function AddUserForm() {
           />
         </div>
         <div className="flex items-center gap-10">  
-          <div class="flex">
-            <div class="flex items-center h-5">
+          <div className="flex">
+            <div className="flex items-center h-5">
               <input
                 id="helper-radio"
                 aria-describedby="helper-radio-text"
@@ -87,26 +87,26 @@ export default function AddUserForm() {
                 type="radio"
                 name="status"
                 value="Active"
-                class="w-4 h-4 cursor-pointer text-green-600 bg-gray-100 border-gray-300  ring-inset focus:ring-green-500"
+                className="w-4 h-4 cursor-pointer text-green-600 bg-gray-100 border-gray-300  ring-inset focus:ring-green-500"
               />
             </div>
-            <div class="ml-2 text-sm">
+            <div className="ml-2 text-sm">
               <label
                 for="helper-radio"
-                class="text-gray-900"
+                className="text-gray-900"
               >
                 Contratar
               </label>
               <p
                 id="helper-radio-text"
-                class="text-xs font-normal text-gray-500"
+                className="text-xs font-normal text-gray-500"
               >
                 Hacer contratos o contratas de un trabajo.
               </p>
             </div>
           </div>
-          <div class="flex">
-            <div class="flex items-center h-5">
+          <div className="flex">
+            <div className="flex items-center h-5">
               <input
                 id="helper-radio"
                 onChange={setFormData}
@@ -114,19 +114,19 @@ export default function AddUserForm() {
                 type="radio"
                 value="Inactive"
                 name="status"
-                class="w-4 h-4 text-green-600 cursor-pointer bg-gray-100 border-gray-300  ring-inset focus:ring-green-500"
+                className="w-4 h-4 text-green-600 cursor-pointer bg-gray-100 border-gray-300  ring-inset focus:ring-green-500"
               />
             </div>
-            <div class="ml-2 text-sm">
+            <div className="ml-2 text-sm">
               <label
                 for="helper-radio"
-                class="text-gray-900"
+                className="text-gray-900"
               >
                 Rescindir
               </label>
               <p
                 id="helper-radio-text"
-                class="text-xs font-normal text-gray-500"
+                className="text-xs font-normal text-gray-500"
               >
                 Dejar sin efecto un contrato u otra obligación legal.
               </p>
