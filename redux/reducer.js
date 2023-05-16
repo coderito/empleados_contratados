5af5b042d4536e5add53e19d77578257435974cc
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-05-16 19:31:29 -0400

    createSlice

---
+++ redux/reducer.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  client: { toggleForm: false },
+};
+
+export const reducerSlice = createSlice({
+  name: "crudapp",
+  initialState,
+  reducers: {
+    toggleChangeAction: (state) => {
+        state.client.toggleForm = !state.client.toggleForm
+    }
+  },
+});
+
+export const {toggleChangeAction} = reducerSlice.actions
+
+export default reducerSlice.reducer
