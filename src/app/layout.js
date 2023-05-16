750ffa563e4367c7cc587c12da1610ce575d3f91
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-05-16 19:32:19 -0400

    import Provider & store

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,9 @@ import "./globals.css";
 import { Poppins } from "next/font/google";
 import { QueryClientProvider, QueryClient} from 'react-query'
 
+import {store} from "../../redux/store"
+import { Provider } from "react-redux";
+
 const poppins = Poppins({ weight: '400', subsets: ["latin"], variable: '--font-poppins' });
 
 const queryClient = new QueryClient();
@@ -11,9 +14,11 @@ const queryClient = new QueryClient();
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <QueryClientProvider client={queryClient}>
-        <body className={`${poppins.variable} font-sans`}>{children}</body>
-      </QueryClientProvider>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <body className={`${poppins.variable} font-sans`}>{children}</body>
+        </QueryClientProvider>
+      </Provider>
     </html>
   );
 }
