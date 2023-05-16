9636060944344e5630f672743789471c823d87da
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-05-16 18:14:53 -0400

    ocupando useQuery

--- components/table.js
+++ components/table.js
@@ -1,21 +1,29 @@
 import { BiEdit, BiTrash } from "react-icons/bi";
-import data from "../database/data.json";
+import { getUsers } from "../lib/helpers";
+import { useQuery } from "react-query";
 
 export default function Table() {
+
+  const {isLoading, isError, data, error} = useQuery('users', getUsers)
+
+  if(isLoading) return <>Cargando empleados..</>
+
+  if(isError) return <>Error {error}</>
+
   function ScopeTableRowItem({ id, name, email, status, salary, date }) {
     return (
-      <tr class="border-b border-gray-200 dark:border-gray-700">
+      <tr className="border-b border-gray-200 dark:border-gray-700">
         <th
           scope="row"
-          class="px-6 py-4 font-bold text-xl text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+          className="px-6 py-4 font-bold text-xl text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
         >
           {name || "Unknown"}
         </th>
-        <td class="px-6 py-4">{email || "Unknown"}</td>
-        <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+        <td className="px-6 py-4">{email || "Unknown"}</td>
+        <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
           {date || "Unknown"}
         </td>
-        <td class="px-6 py-4">
+        <td className="px-6 py-4">
           {status === "Active" ? (
             <button className="bg-green-400 text-white px-3 py-1 rounded-sm">
               {status || "Unknown"}
@@ -26,10 +34,10 @@ export default function Table() {
             </button>
           )}
         </td>
-        <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+        <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
           {salary || "Unknown"}
         </td>
-        <td class="px-6 py-4 flex justify-around">
+        <td className="px-6 py-4 flex justify-around">
           <BiEdit size={25} className="text-green-600" />
           <BiTrash size={25} className="text-red-600" />
         </td>
@@ -38,26 +46,26 @@ export default function Table() {
   }
 
   return (
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
           <tr className="bg-black text-white">
-            <th scope="col" class="px-6 py-3 ">
+            <th scope="col" className="px-6 py-3 ">
               Nombre
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Email
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Fecha de Ingreso
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Estado
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Sueldo
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Acciones
             </th>
           </tr>
